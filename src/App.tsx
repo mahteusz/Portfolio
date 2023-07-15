5bd9646c708fd977bdf33b79ed8e259f16394e8d
Mateus Marques <mateusmarques200@gmail.com>
2023-07-15 02:22:46 -0300

    FIX: removing deleted component from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import Container from './Components/Container'
 import ToggleSwitch from './Components/ToggleSwitch'
 import { ThemeProvider } from './Contexts'
-import { useTheme } from './Contexts/useTheme'
 
 const App = () => {
-  const themeData = useTheme()
 
   return (
     <ThemeProvider>
-      <Container />
+      <ToggleSwitch onChange={() => {}}/>
     </ThemeProvider>
   )
 }
