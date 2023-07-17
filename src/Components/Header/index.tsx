581bb4f72d475fc26cde37c491285cf2af82e076
Mateus Marques <mateusmarques200@gmail.com>
2023-07-17 17:36:27 -0300

    FEAT: finishing navigation menu UI and animations

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,15 +3,36 @@ import { useTheme } from '../../Contexts/useTheme'
 import './styles.scss'
 
 const Header = () => {
-  const [navbarOpen, setNavbarOpen] = useState<boolean>(false) 
+  const [navbarOpen, setNavbarOpen] = useState<boolean>(false)
   const themeData = useTheme()
 
+  const exitOnOuterClick = (e: React.MouseEvent<HTMLElement>) => {
+    if(e.target != e.currentTarget) 
+      return
+    
+    setNavbarOpen(false)
+  }
+
   return (
-    <header className={`${themeData.theme}`} onClick={() => setNavbarOpen(!navbarOpen)}>
-      <div className={`hamburguer ${navbarOpen ? 'active' : ''}`}>
+    <header className={`${themeData.theme}`}>
+      <div 
+        className={`hamburguer`}
+        onClick={() => setNavbarOpen(!navbarOpen)}
+      >
         <div className={`hamburguer__line ${themeData.theme}`} />
         <div className={`hamburguer__line ${themeData.theme}`} />
         <div className={`hamburguer__line ${themeData.theme}`} />
+      </div>
+      <div 
+        className={`menu ${navbarOpen ? 'open' : ''}`}
+        onClick={exitOnOuterClick}
+      >
+        <nav className={`menu__navigation ${themeData.theme} ${navbarOpen ? 'open' : ''}`}>
+          <li className={`menu__navigation-item`}>Projetos</li>
+          <li className={`menu__navigation-item`}>Projetos</li>
+          <li className={`menu__navigation-item`}>Projetos</li>
+          <li className={`menu__navigation-item`}>Projetos</li>
+        </nav>
       </div>
     </header>
   )
