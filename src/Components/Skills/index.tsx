98f4d2b3e525ee2df870495da1241cc52be4ead8
Mateus Marques <mateusmarques200@gmail.com>
2023-08-02 02:02:51 -0300

    FEAT: creating SvgIcon component and finishing skills section

--- src/Components/Skills/index.tsx
+++ src/Components/Skills/index.tsx
@@ -1,6 +1,8 @@
 import './styles.scss'
 import { useTheme } from '../../Contexts'
-import HTML5 from '../../assets/icons/html5.svg'
+import { HTML5, CSS3, JS, TS, ReactLogo, StyledComponents, SASS, Node, Express, MongoDB, Git, Jest }
+  from '../../assets/icons'
+import { SvgIconProps } from './types'
 
 const Skills = () => {
 
@@ -12,14 +14,34 @@ const Skills = () => {
         habilidades
       </h1>
       <div className={`skills-container__cards ${themeData.theme}`}>
-        <div className={`skills-container__card ${themeData.theme}`}>
-          <h1 className={`skills-container__card-name ${themeData.theme}`}>
-            HTML5
-          </h1>
-          <img className={`skills-container__card-icon ${themeData.theme}`} src={HTML5} />
-        </div>
+        <SvgIcon innerText='HTML5' Icon={HTML5} />
+        <SvgIcon innerText='CSS3' Icon={CSS3} />
+        <SvgIcon innerText='JavaScript' Icon={JS} />
+        <SvgIcon innerText='TypeScript' Icon={TS} />
+        <SvgIcon innerText='React' Icon={ReactLogo} />
+        <SvgIcon innerText='Styled Components' Icon={StyledComponents} />
+        <SvgIcon innerText='SASS' Icon={SASS} />
+        <SvgIcon innerText='Node' Icon={Node} />
+        <SvgIcon innerText='Express' Icon={Express} />
+        <SvgIcon innerText='MongoDB' Icon={MongoDB} />
+        <SvgIcon innerText='Git' Icon={Git} />
+        <SvgIcon innerText='Jest' Icon={Jest} />
       </div>
     </section>
+  )
+}
+
+const SvgIcon = ({ innerText, Icon }: SvgIconProps) => {
+
+  const themeData = useTheme()
+
+  return (
+    <div className={`skills-container__card ${themeData.theme}`}>
+      <h1 className={`skills-container__card-name ${themeData.theme}`}>
+        {innerText}
+      </h1>
+      <Icon className={`skills-container__card-icon`} />
+    </div>
   )
 }
 
