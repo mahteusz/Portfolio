68e0dc5bf63c9250a7a750bb3439a56311ed59b3
Mateus Marques <mateusmarques200@gmail.com>
2023-08-02 23:19:03 -0300

    FEAT: adding links to skill cards

--- src/Components/Skills/types.ts
+++ src/Components/Skills/types.ts
@@ -1,6 +1,7 @@
 type SvgIconProps = {
   innerText: string,
-  Icon: React.FC<React.SVGProps<SVGSVGElement>>
+  Icon: React.FC<React.SVGProps<SVGSVGElement>>,
+  href: string
 }
 
 export { type SvgIconProps }
